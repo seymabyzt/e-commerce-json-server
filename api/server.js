635ef52fa595d18e7c51828078aa9da00781fae9
Seyma Bayezit <163021404+seymabyzt@users.noreply.github.com>
2024-07-11 22:57:19 +0300

    json server

---
+++ api/server.js
@@ -0,0 +1,27 @@
+// See https://github.com/typicode/json-server#module
+const jsonServer = require('json-server')
+
+const server = jsonServer.create()
+
+// Uncomment to allow write operations
+// const fs = require('fs')
+// const path = require('path')
+// const filePath = path.join('db.json')
+// const data = fs.readFileSync(filePath, "utf-8");
+// const db = JSON.parse(data);
+// const router = jsonServer.router(db)
+
+// Comment out to allow write operations
+const router = jsonServer.router('db.json')
+
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+
+server.use(router)
+server.listen(3000, () => {
+    console.log('JSON Server is running')
+})
+
+// Export the Server API
+module.exports = server
